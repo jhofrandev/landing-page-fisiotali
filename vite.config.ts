90b3a6af6708dff515b99d915e91cfabdf5df364
jhofrandev <jhofran.dev@outlook.com>
2026-02-19 09:44:46 -0500

    feat: Add Flowbite React library and configure project for its integration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "path";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    flowbiteReact()
   ],
   resolve: {
     alias: {
