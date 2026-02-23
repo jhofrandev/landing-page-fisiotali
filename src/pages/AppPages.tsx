c63ebd035b1a20c0a84f7ff75663274252a8797c
jhofrandev <jhofran.dev@outlook.com>
2026-02-23 11:50:26 -0500

    add change

--- src/pages/AppPages.tsx
+++ src/pages/AppPages.tsx
@@ -1,36 +1,92 @@
-import { Link, Outlet } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export default function AppPages() {
   return (
     <>
-      <header className="p-4 flex justify-between items-center">
-        <img
-          src="https://fisiotali.sirv.com/landing/images/logo-mini.png"
-          alt="logo de fisiotali"
-          className="w-24"
-        />
-        <nav>
-          <ul className="flex gap-4">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="/#about">About</a>
-            </li>
-            <li>
-              <a href="/#services">Services</a>
-            </li>
-            <li>
-              <a href="/#contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <button className="bg-lime-500 text-white px-4 py-2 rounded">
-          Agendar Cita
-        </button>
-      </header>
-      <Outlet />
+      <nav className="bg-neutral-primary border-b border-default">
+        <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://fisiotali.sirv.com/landing/images/logo-mini.png"
+              alt="logo de fisiotali"
+              className="w-24"
+            />
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="M5 7h14M5 12h14M5 17h14"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 items-center border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+              <li>
+                <a
+                  href="/"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#about"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Nosotros
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#services"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Servicios
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#contact"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Contacto
+                </a>
+              </li>
+              <li>
+                <button className="bg-lime-500 text-white px-4 py-2 rounded-lg hover:bg-lime-600 transition-colors">
+                  Agendar Cita
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main className="">
+        <Outlet />
+      </main>
       <TanStackRouterDevtools />
     </>
   );
--- src/templates/HomeTemplate.tsx
+++ src/templates/HomeTemplate.tsx
@@ -1,3 +1,39 @@
+import { Carousel, CustomFlowbiteTheme } from "flowbite-react";
+
+const customTheme: CustomFlowbiteTheme["carousel"] = {
+  indicators: {
+    base: "h-3 w-3 rounded-full",
+    active: {
+      off: "bg-white/50 hover:bg-white dark:bg-gray-800/50 dark:hover:bg-gray-800",
+      on: "bg-white dark:bg-gray-800",
+    },
+    wrapper: "absolute bottom-2 left-1/2 flex -translate-x-1/2 space-x-3",
+  },
+};
+
 export default function HomeTemplate() {
-  return <div>HomeTemplate</div>;
+  const images = [
+    "https://ayudawp.com/wp-content/uploads/2017/12/featured-social-media-image-size.png",
+    "https://ayudawp.com/wp-content/uploads/2017/12/featured-social-media-image-size.png",
+    "https://ayudawp.com/wp-content/uploads/2017/12/featured-social-media-image-size.png",
+    "https://ayudawp.com/wp-content/uploads/2017/12/featured-social-media-image-size.png",
+    "https://ayudawp.com/wp-content/uploads/2017/12/featured-social-media-image-size.png",
+  ];
+
+  return (
+    <>
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slide={true} theme={customTheme}>
+          {images.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+          ))}
+        </Carousel>
+      </div>
+    </>
+  );
 }
